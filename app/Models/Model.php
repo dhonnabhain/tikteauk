7f37021d0fd9945ea36feebef0789c0107662e93
Donovan <donovan.broquin@outlook.com>
2020-09-23 10:19:26 +0200

    feat: add all method in model with table name binder from class

--- app/Models/Model.php
+++ app/Models/Model.php
@@ -1,7 +1,32 @@
 <?php
 
-namespace App\Model;
+namespace App\Models;
+
+use PDO;
+use Symfony\Component\String\Inflector\EnglishInflector;
 
 abstract class Model
 {
+    private string $table;
+
+    public function __construct()
+    {
+        $this->extractTableName();
+    }
+
+    public function all()
+    {
+        $query = $GLOBALS['connection']->prepare("SELECT * FROM $this->table");
+        $query->execute();
+
+        return $query->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    private function extractTableName(): void
+    {
+        $namespace = get_class($this);
+        $classname = array_reverse(explode('\\', $namespace))[0];
+
+        $this->table = strtoupper((new EnglishInflector)->pluralize($classname)[0]);
+    }
 }
